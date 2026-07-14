acd299f92a24ec9eeb70ce1e1bc470f806bbdb60
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:37:56 +0530

    Create 921-SpiralMatrixIii

---
+++ https:/github.com/HarshRana446/Leet/921-SpiralMatrixIii/921-SpiralMatrixIii.java
@@ -0,0 +1,31 @@
+// Last updated: 7/14/2026, 2:37:54 PM
+class Solution {
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int[][] result = new int[rows * cols][2];
+        int index = 0;
+        int row = rStart;
+        int col = cStart;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int direction = 0;
+        int steps = 1;
+        int stepCount = 0;
+        while (index < rows * cols) {
+            if (row >= 0 && row < rows && col >= 0 && col < cols) {
+                result[index][0] = row;
+                result[index][1] = col;
+                index++;
+            }
+            row += directions[direction][0];
+            col += directions[direction][1];
+            stepCount++;
+            if (stepCount == steps) {
+                stepCount = 0;
+                direction = (direction + 1) % 4;
+                if (direction % 2 == 0) {
+                    steps++;
+                }
+            }
+        }
+        return result;
+    }
+}
