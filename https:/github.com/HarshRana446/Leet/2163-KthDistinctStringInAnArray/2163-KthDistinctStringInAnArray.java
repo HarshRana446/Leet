fe8c93430515bee18254f1d4e330411454303853
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:36:51 +0530

    Create 2163-KthDistinctStringInAnArray

---
+++ https:/github.com/HarshRana446/Leet/2163-KthDistinctStringInAnArray/2163-KthDistinctStringInAnArray.java
@@ -0,0 +1,28 @@
+// Last updated: 7/14/2026, 2:36:50 PM
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        Map<String, Integer> frequencyMap = new HashMap<>();
+
+        // Count occurrences of each string in the array
+        for (String str : arr) {
+            frequencyMap.put(str, frequencyMap.getOrDefault(str, 0) + 1);
+        }
+
+        // Traverse the array to find the kth distinct string
+        int distinctCount = 0;
+        for (String str : arr) {
+            if (frequencyMap.get(str) == 1) {
+                distinctCount++;
+                if (distinctCount == k) {
+                    return str;
+                }
+            }
+        }
+
+        // If there is no kth distinct string, return an empty string
+        return "";
+    }
+}
