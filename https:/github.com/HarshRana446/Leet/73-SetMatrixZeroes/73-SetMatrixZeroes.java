1572847fdc70d3f58447335557d6944bf5dd2783
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:39:28 +0530

    Create 73-SetMatrixZeroes

---
+++ https:/github.com/HarshRana446/Leet/73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -0,0 +1,44 @@
+// Last updated: 7/14/2026, 2:39:26 PM
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        //int col[m] = matrix[0][];
+        //int col[n] = matrix[][0];
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int col0 = 1;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    // mark ith = 0;
+                    matrix[i][0] = 0;
+                    // mark jth = 0;
+                    if(j != 0){
+                        matrix[0][j] = 0;
+                    }else{
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] != 0){
+                    // check the row & col
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+        if(matrix[0][0] == 0){
+            for(int j = 0; j < n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(col0 == 0){
+            for(int i = 0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
