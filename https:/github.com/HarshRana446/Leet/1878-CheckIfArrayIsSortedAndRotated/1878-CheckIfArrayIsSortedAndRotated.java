05ff8b587bdf4aed83b9ffe56c6e94e4ca21edc0
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:36:59 +0530

    Create 1878-CheckIfArrayIsSortedAndRotated

---
+++ https:/github.com/HarshRana446/Leet/1878-CheckIfArrayIsSortedAndRotated/1878-CheckIfArrayIsSortedAndRotated.java
@@ -0,0 +1,12 @@
+// Last updated: 7/14/2026, 2:36:58 PM
+class Solution {
+    public boolean check(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > nums[(i + 1) % nums.length]){
+                count++;
+            }
+        }
+        return count <= 1;
+    }
+}
