d6bd5cddde1d157fda0500b321ae45a5476be699
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:50:37 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.js
@@ -0,0 +1,19 @@
+// Last updated: 7/14/2026, 2:50:36 PM
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if(n <= 2){
+        return n;
+    }
+    let one = 2;
+    let two = 1;
+
+    for(let i = 3; i <= n; i++){
+        let current = one + two;
+        two = one;
+        one = current;
+    }
+    return one;
+};
