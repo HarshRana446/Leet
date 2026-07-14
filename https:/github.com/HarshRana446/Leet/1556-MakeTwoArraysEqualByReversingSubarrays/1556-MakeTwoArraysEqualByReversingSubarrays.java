3d6e4e5a6c64742abfa33a23013cf93269e94682
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:37:34 +0530

    Create 1556-MakeTwoArraysEqualByReversingSubarrays

---
+++ https:/github.com/HarshRana446/Leet/1556-MakeTwoArraysEqualByReversingSubarrays/1556-MakeTwoArraysEqualByReversingSubarrays.java
@@ -0,0 +1,19 @@
+// Last updated: 7/14/2026, 2:37:33 PM
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        HashMap<Integer, Integer> _Map = new HashMap<>();
+        for(int t : target){
+            int count = _Map.getOrDefault(t, 0);
+            _Map.put(t, count+1);
+        }
+
+        for(int a: arr){
+            if (_Map.containsKey(a) && _Map.get(a)>0) {
+                _Map.put(a, _Map.get(a)-1);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
