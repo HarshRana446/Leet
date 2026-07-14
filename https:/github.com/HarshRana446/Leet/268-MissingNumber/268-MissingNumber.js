594f13034fecf5c3ebda221c9305a9c23427fac5
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:38:53 +0530

    Create 268-MissingNumber

---
+++ https:/github.com/HarshRana446/Leet/268-MissingNumber/268-MissingNumber.js
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 2:38:53 PM
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let n = nums.length;
+    let expectedSum = n * (n+1)/2;
+    let sum = 0;
+    for(let i = 0; i < n; i++){
+        sum += nums[i];
+    }
+    return expectedSum - sum;
+};
