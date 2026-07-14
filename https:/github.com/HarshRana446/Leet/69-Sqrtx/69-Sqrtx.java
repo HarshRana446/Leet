56f8f6bdcb636b01bcebd63646ecee7410c16b5b
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:39:24 +0530

    Create 69-Sqrtx

---
+++ https:/github.com/HarshRana446/Leet/69-Sqrtx/69-Sqrtx.java
@@ -0,0 +1,29 @@
+// Last updated: 7/14/2026, 2:39:23 PM
+class Solution {
+    public int mySqrt(int x) {
+                int ans = 0;
+        if(x == 0){
+            return 0;
+        } else if (x==1){
+            return 1;
+        }
+        else {
+        int left = 0;
+        int right = x;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid == x){
+                return mid;
+            }
+            else if ((long)mid * mid < x){
+                ans = mid;
+                left = mid + 1;
+            }
+            else if ((long)mid * mid > x){
+                right = mid - 1;
+            }
+        }
+        }
+        return ans;
+    }
+}
