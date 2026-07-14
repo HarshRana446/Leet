051e686b8ffbf89921d2076ea42cebc6bd56a80f
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:39:32 +0530

    Create 66-PlusOne

---
+++ https:/github.com/HarshRana446/Leet/66-PlusOne/66-PlusOne.js
@@ -0,0 +1,17 @@
+// Last updated: 7/14/2026, 2:39:31 PM
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+
+    digits.unshift(1);
+    return digits;
+};
