ae6229e6b85b8505bad9d6ffa182c25f20986b4d
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:39:00 +0530

    Create 229-MajorityElementIi

---
+++ https:/github.com/HarshRana446/Leet/229-MajorityElementIi/229-MajorityElementIi.java
@@ -0,0 +1,35 @@
+// Last updated: 7/14/2026, 2:38:59 PM
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+    int count1 = 0; int count2 = 0;
+    int el1 = Integer.MIN_VALUE;
+    int el2 = Integer.MIN_VALUE;
+    for(int i = 0; i < nums.length; i++){
+        if(count1 == 0 && el2 != nums[i]){
+            count1 = 1;
+            el1 = nums[i];
+        }
+        else if(count2 == 0 && el1 != nums[i]){
+            count2 = 1;
+            el2 = nums[i];
+        }
+        else if(nums[i] == el1) count1++;
+        else if(nums[i] == el2) count2++;
+        else{
+            count1--;
+            count2--;
+        }
+    }
+    ArrayList<Integer> list = new ArrayList<>();
+    count1= 0; count2 = 0;
+    for(int i = 0; i< nums.length; i++){
+        if(el1 == nums[i]) count1++;
+        if(el2 == nums[i]) count2++;
+    }
+    int mini = (int) nums.length / 3 + 1;
+    if(count1 >= mini) list.add(el1);
+    if(count2 >= mini) list.add(el2);
+    Collections.sort(list);
+    return list;
+    }
+}
