9c967dc461e489cafe3d5593523dbc31a4708bc5
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:40:19 +0530

    Create 14-LongestCommonPrefix

---
+++ https:/github.com/HarshRana446/Leet/14-LongestCommonPrefix/14-LongestCommonPrefix.java
@@ -0,0 +1,19 @@
+// Last updated: 7/14/2026, 2:40:19 PM
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder res = new StringBuilder();
+
+        Arrays.sort(strs);
+
+        char[] first = strs[0].toCharArray();
+        char[] last = strs[strs.length - 1].toCharArray();
+
+        for(int i=0; i<first.length ; i++){
+            if(first[i] != last[i]){
+                break;
+            }
+             res.append(first[i]);
+        }
+        return res.toString();
+    }
+}
