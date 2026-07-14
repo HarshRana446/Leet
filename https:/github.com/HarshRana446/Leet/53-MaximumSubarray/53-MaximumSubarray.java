178e05b9db084bb900643fab5d5f2ea720361ec9
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:39:42 +0530

    Create 53-MaximumSubarray

---
+++ https:/github.com/HarshRana446/Leet/53-MaximumSubarray/53-MaximumSubarray.java
@@ -0,0 +1,17 @@
+// Last updated: 7/14/2026, 2:39:42 PM
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i <= nums.length - 1; i++){
+            sum += nums[i];
+            if(sum > max){
+                max = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
