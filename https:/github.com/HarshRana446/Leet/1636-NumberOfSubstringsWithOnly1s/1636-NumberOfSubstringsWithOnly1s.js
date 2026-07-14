438dd4de503a2d03438ce19d274c1f496b10502b
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:37:32 +0530

    Create 1636-NumberOfSubstringsWithOnly1s

---
+++ https:/github.com/HarshRana446/Leet/1636-NumberOfSubstringsWithOnly1s/1636-NumberOfSubstringsWithOnly1s.js
@@ -0,0 +1,9 @@
+// Last updated: 7/14/2026, 2:37:31 PM
+function numSub(s) {
+    let total = 0;
+    s.split("0").filter(block => {
+        const length = block.length;
+        if (length) total += (length * (length + 1)) / 2
+    });
+    return total % (10 ** 9 + 7);
+};
