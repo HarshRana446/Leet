00fe5f80a04d9f6279f6242397be6e967bc83dad
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:39:02 +0530

    Create 217-ContainsDuplicate

---
+++ https:/github.com/HarshRana446/Leet/217-ContainsDuplicate/217-ContainsDuplicate.js
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 2:39:02 PM
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    nums.sort((a,b) => a-b)
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i]  == nums[i-1]){
+            return true;
+        }  
+    }
+    return false;
+};
