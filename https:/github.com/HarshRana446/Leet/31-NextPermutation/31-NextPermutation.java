ba563c2c7e061164d3d030cbc1b80874007f57cd
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-14 14:39:54 +0530

    Create 31-NextPermutation

---
+++ https:/github.com/HarshRana446/Leet/31-NextPermutation/31-NextPermutation.java
@@ -0,0 +1,48 @@
+// Last updated: 7/14/2026, 2:39:52 PM
+class Solution {
+
+    public void nextPermutation(int[] nums) {
+
+        int n = nums.length;
+        int ind = -1;
+
+        // Step 1: Find the breakpoint
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+
+        // Step 2: If no breakpoint, reverse the whole array
+        if (ind == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+
+        // Step 3: Find the next greater element
+        for (int i = n - 1; i > ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(nums, i, ind);
+                break;
+            }
+        }
+
+        // Step 4: Reverse the suffix
+        reverse(nums, ind + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
