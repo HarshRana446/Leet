4a82e9e719a35f9c7e1aeafad1e58d752c7e5aa9
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-07-22 14:33:46 +0530

    Create 240-SearchA2dMatrixIi

---
+++ 240-SearchA2dMatrixIi/240-SearchA2dMatrixIi.java
@@ -0,0 +1,19 @@
+// Last updated: 7/22/2026, 2:33:45 PM
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = 0;
+        int col = m - 1;
+        while(row < n && col >= 0){
+            if(matrix[row][col] == target){
+                return true;
+            }else if(matrix[row][col] < target){
+                row++;
+            }else{
+                col--;
+            }
+        }
+        return false;
+    }
+}
