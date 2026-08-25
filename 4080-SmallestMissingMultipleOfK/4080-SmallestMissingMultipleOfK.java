57793b9ed85c8652027633387dbfaaeba3abaed4
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-08-25 17:13:21 +0530

    Create 4080-SmallestMissingMultipleOfK

---
+++ 4080-SmallestMissingMultipleOfK/4080-SmallestMissingMultipleOfK.java
@@ -0,0 +1,18 @@
+// Last updated: 8/25/2026, 5:13:13 PM
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int multiple = k;
+
+        while (set.contains(multiple)) {
+            multiple += k;
+        }
+
+        return multiple;
+    }
+}
