41d3133e9255c9596f7052d50e1f96ebbdd0377f
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-09-07 14:43:07 +0530

    Create 1365-HowManyNumbersAreSmallerThanTheCurrentNumber

---
+++ 1365-HowManyNumbersAreSmallerThanTheCurrentNumber/1365-HowManyNumbersAreSmallerThanTheCurrentNumber.js
@@ -0,0 +1,18 @@
+// Last updated: 9/7/2026, 2:43:06 PM
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function(nums) {
+    let result = [];
+    for(let i = 0; i < nums.length; i++){
+        let count = 0;
+        for(let j = 0; j < nums.length; j++){
+            if(nums[j] < nums[i]){
+                count++;
+            }
+        }
+    result.push(count);
+    }
+return result;
+};
