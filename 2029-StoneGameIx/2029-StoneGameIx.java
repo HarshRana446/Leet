88258fc3b79dbc051c63faf43067120103bb9d26
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-09-07 14:41:50 +0530

    Create 2029-StoneGameIx

---
+++ 2029-StoneGameIx/2029-StoneGameIx.java
@@ -0,0 +1,16 @@
+// Last updated: 9/7/2026, 2:41:50 PM
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] cnt = new int[3];
+
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        if (cnt[0] % 2 == 0) {
+            return cnt[1] > 0 && cnt[2] > 0;
+        }
+
+        return Math.abs(cnt[1] - cnt[2]) > 2;
+    }
+}
