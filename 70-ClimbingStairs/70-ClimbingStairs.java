8edea8a471e8da2b6e74942ba38ed8e9a26a9d7e
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-09-09 13:29:42 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,23 @@
+// Last updated: 9/9/2026, 1:29:41 PM
+class Solution {
+
+    public int climbStairs(int n) {
+
+        if (n <= 2) {
+            return n;
+        }
+
+        int prev2 = 1;
+        int prev1 = 2;
+
+        for (int i = 3; i <= n; i++) {
+
+            int current = prev1 + prev2;
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+}
