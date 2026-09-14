cb9734306f3657e09f7f0480e2ee23ba88224d8b
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-09-14 13:32:16 +0530

    Create 836-RectangleOverlap

---
+++ 836-RectangleOverlap/836-RectangleOverlap.java
@@ -0,0 +1,15 @@
+// Last updated: 9/14/2026, 1:32:15 PM
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        if (rec1[2] <= rec2[0] ||
+                rec2[2] <= rec1[0] ||
+                rec1[3] <= rec2[1] ||
+                rec2[3] <= rec1[1]) {
+
+            return false;
+        }
+
+        return true;
+
+    }
+}
