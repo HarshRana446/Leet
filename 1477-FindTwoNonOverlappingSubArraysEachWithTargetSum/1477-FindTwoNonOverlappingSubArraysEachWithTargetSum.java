f6829c43398f2111bd779c090d249370b8e8dbc9
HarshRana446 <142232736+HarshRana446@users.noreply.github.com>
2026-09-17 14:27:50 +0530

    Create 1477-FindTwoNonOverlappingSubArraysEachWithTargetSum

---
+++ 1477-FindTwoNonOverlappingSubArraysEachWithTargetSum/1477-FindTwoNonOverlappingSubArraysEachWithTargetSum.java
@@ -0,0 +1,39 @@
+// Last updated: 9/17/2026, 2:27:49 PM
+class Solution {
+    public int minSumOfLengths(int[] arr, int target) {
+        int n = arr.length;
+        int[] best = new int[n + 1];
+
+        int INF = n + 1;
+
+        for (int i = 0; i <= n; i++) {
+            best[i] = INF;
+        }
+
+        int left = 0;
+        int sum = 0;
+        int ans = INF;
+
+        for (int right = 0; right < n; right++) {
+            sum += arr[right];
+
+            while (sum > target) {
+                sum -= arr[left++];
+            }
+
+            if (sum == target) {
+                int len = right - left + 1;
+
+                if (best[left] != INF) {
+                    ans = Math.min(ans, len + best[left]);
+                }
+
+                best[right + 1] = Math.min(best[right], len);
+            } else {
+                best[right + 1] = best[right];
+            }
+        }
+
+        return ans == INF ? -1 : ans;
+    }
+}
